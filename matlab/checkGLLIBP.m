42bc74df16bc0710574d566005a44b2df3ea8b28
Jesse Chan <jchan985@gmail.com>
2017-01-24 10:24:51 -0600

    adding old wedge experiments

--- matlab/checkGLLIBP.m
+++ matlab/checkGLLIBP.m
@@ -1,15 +1,60 @@
+N = 10;
+rp = linspace(-1,1,1000)';
+Vp = Vandermonde1D(N,rp);
 [r w] = JacobiGL(0,0,N);
 V = Vandermonde1D(N,r);
 Vr = GradVandermonde1D(N,r);
 Dr = Vr/V;
-% M = inv(V*V'); 
-M = diag(w); 
+M = inv(V*V');
+% M = diag(w);
 f = randn(N+1,1);
-J = rand(N+1,1)+.5;
+J = 1+randn(N+1,1);
 
-fb = zeros(N+1,1); fb(1) = -f(1)*J(1); fb(N+1) = f(N+1)*J(N+1);
-
-rhs1 = diag(J)*M*Dr*f;
-rhs2 = fb-diag(J)*Dr'*M*f;
+B = diag([-1;zeros(N-1,1);1]);
+rhs1 = diag(J)*M*(Dr*f);
+rhs2 = diag(J)*B*f - (Dr*diag(J))'*M*f;
 
 rhs1-rhs2
+
+
+%% check 2D version
+N = 4;
+r1D = JacobiGL(0,0,N);
+[r1Dq  w1Dq] = JacobiGQ(0,0,N);
+[r s] = meshgrid(r1D);
+r = r(:); s = s(:);
+
+rfq = [r1Dq; ones(N+1,1); -r1Dq; -ones(N+1,1)];
+sfq = [-ones(N+1,1); r1Dq; ones(N+1,1); -r1Dq];
+wfq = repmat(w1Dq,4,1);
+
+V = zeros((N+1)^2);
+Vr = zeros((N+1)^2);
+Vs = zeros((N+1)^2);
+Vfq = zeros(length(wfq),(N+1)^2);
+sk = 1;
+for i = 0:N
+    for j = 0:N
+        V(:,sk) = JacobiP(r,0,0,i).*JacobiP(s,0,0,j);
+        Vr(:,sk) = GradJacobiP(r,0,0,i).*JacobiP(s,0,0,j);
+        Vs(:,sk) = JacobiP(r,0,0,i).*GradJacobiP(s,0,0,j);
+        Vfq(:,sk) = JacobiP(rfq,0,0,i).*JacobiP(sfq,0,0,j);        
+        sk = sk + 1;
+    end
+end
+Dr = Vr/V;
+Ds = Vs/V;
+Vfq = Vfq/V;
+
+M = inv(V*V'); 
+% M = diag(sum(M,2)); % lump
+Mf = Vfq'*diag(wfq)*Vfq;
+
+f = randn(N+1); 
+f = f(:);
+f = (1+r).*(1+s).*(1-r).*(1-s);
+
+rhs1 = M*Dr*f;
+rhs2 = Mf*f - Dr'*M*f;
+
+norm(rhs1-rhs2)
--- matlab/checkWedgePyrLifts.m
+++ matlab/checkWedgePyrLifts.m
@@ -1,23 +1,42 @@
 % wedges
 clear
-N = 4;
+N = 2;
 
 VX = [-1 1 -1 -1 1 -1]'; VY = [-1 -1 -1 1 1 1]'; VZ = [-1 -1 1 -1 -1 1]';
 % plot3(VX,VY,VZ,'o'); text(VX,VY,VZ,num2str((1:6)'));return
 
-a = .2;
+a = .33;
 VX = VX+a*randn(length(VX),1);
 VY = VY+a*randn(length(VX),1);
 VZ = VZ+a*randn(length(VX),1);
 
-[r s t] = wedge_nodes(N);
+% VY(1) = VY(1)+.5;
+% plot3(VX,VY,VZ,'o-');return
+
+
+% tensor product wedge nodes
+[rtri stri] = Nodes2D(N); [rtri stri] = xytors(rtri,stri);
+r1D = JacobiGL(0,0,N);
+[r1Dq w1Dq] = JacobiGQ(0,0,N);
+
+[~,r] = meshgrid(r1D,rtri); 
+[s,t] = meshgrid(r1D,stri);
+r = r(:); s = s(:); t = t(:);
+
 Np = length(r);
 Nfp = (N+1)^2*3 + (N+1)*(N+2);
 [x,y,z,rx,sx,tx,ry,sy,ty,rz,sz,tz,J] = wedge_geom_factors(VX,VY,VZ,r,s,t);
 
-[rq sq tq wq] = wedge_cubature(N);
-[rfq tfq sfq wfq fids] = wedge_surface_cubature(N);
+% [rq sq tq wq] = wedge_cubature(N);
+[rqtri sqtri wqtri] = Cubature2D(2*N+1);
+[~,rq] = meshgrid(r1Dq,rqtri);
+[sq,tq] = meshgrid(r1Dq,sqtri);
+[wsq,wrtq] = meshgrid(w1Dq,wqtri);
+rq = rq(:); sq = sq(:); tq = tq(:);  wq = wsq(:).*wrtq(:);
 
+
+[rfq tfq sfq wfq fids] = wedge_surface_cubature(N);
+wfq = wfq(:);
 % plot3(r,s,t,'*')
 % plot3(VX,VY,VZ,'o','markersize',10)
 % hold on
@@ -40,15 +59,14 @@ Vp = wedge_basis(N,rp,sp,tp)/V;
 xp = Vp*x; yp = Vp*y; zp = Vp*z;
 
 [Vq Vrq Vsq Vtq] = wedge_basis(N,rq,sq,tq);
-Vqmodal = Vq;
 Vq = Vq/V; Vrq = Vrq/V; Vsq = Vsq/V; Vtq = Vtq/V;
 
-[xq,yq,zq,rxq,sxq,txq,ryq,syq,tyq,rzq,szq,tzq,Jq] = wedge_geom_factors(VX,VY,VZ,rq,sq,tq);
+Vfq = wedge_basis(N,rfq,sfq,tfq)/V;
 
+% vol/surf geofacs
+[xq,yq,zq,rxq,sxq,txq,ryq,syq,tyq,rzq,szq,tzq,Jq] = wedge_geom_factors(VX,VY,VZ,rq,sq,tq);
 [xfq,yfq,zfq,rxf,sxf,txf,ryf,syf,tyf,rzf,szf,tzf,Jf] = wedge_geom_factors(VX,VY,VZ,rfq,sfq,tfq);
-nx = zeros(size(xfq));
-ny = zeros(size(xfq));
-nz = zeros(size(xfq));
+nx = zeros(size(xfq)); ny = zeros(size(xfq)); nz = zeros(size(xfq));
 nx(fids{1}) = -sxf(fids{1}); ny(fids{1}) = -syf(fids{1}); nz(fids{1}) = -szf(fids{1});
 nx(fids{2}) = -txf(fids{2}); ny(fids{2}) = -tyf(fids{2}); nz(fids{2}) = -tzf(fids{2});
 nx(fids{3}) = rxf(fids{3})+txf(fids{3}); 
@@ -56,12 +74,9 @@ ny(fids{3}) = ryf(fids{3})+tyf(fids{3});
 nz(fids{3}) = rzf(fids{3})+tzf(fids{3}); 
 nx(fids{4}) = -rxf(fids{4}); ny(fids{4}) = -ryf(fids{4}); nz(fids{4}) = -rzf(fids{4});
 nx(fids{5}) = sxf(fids{5}); ny(fids{5}) = syf(fids{5}); nz(fids{5}) = szf(fids{5});
-
 sJ = sqrt(nx.^2 + ny.^2 + nz.^2);
 nx = nx./sJ; ny = ny./sJ; nz = nz./sJ;
 sJ = sJ.*Jf;
-
-Vfq = wedge_basis(N,rfq,sfq,tfq)/V;
 
 % plot3(xfq,yfq,zfq,'o')
 % hold on
@@ -77,29 +92,21 @@ f = @(x,y,z) x.^N + y.^N + z.^N;
 % norm(Vp*(rz.*(Dr*f(x,y,z)) + sz.*(Ds*f(x,y,z)) + tz.*(Dt*f(x,y,z)))-dfdz(xp,yp,zp),'fro')
 
 % check IBP
-fq = f(xq,yq,zq);
-ffq = f(xfq,yfq,zfq);
-f = f(x,y,z); % interpolant
-% f = zeros(Np,1); f((N+1)*(N+2)/2+1) = 1;
+f = randn(Np,1);
 
 Mref = Vq'*diag(wq)*Vq;
 LHSnodal = Mref*(J.*(rx.*(Dr*f) + sx.*(Ds*f) + tx.*(Dt*f)));
 
-Mf = Vfq'*diag(wfq)*Vfq;
-LHSnodalIBP = Vfq'*diag(wfq.*nx)*Vfq*f - (diag(J.*rx)*Dr' + diag(J.*sx)*Ds' + diag(J.*tx)*Dt')*Mref*f;
-
-% LHSex = Vq'*(Jq.*wq.*dfdx(xq,yq,zq));
-% LHSIBPex = -(Vrq'*(rxq.*Jq.*wq.*(fq)) + Vsq'*(sxq.*Jq.*wq.*(fq)) + Vtq'*(txq.*Jq.*wq.*(fq)));
-% LHSIBPex = LHSIBPex + Vfq'*(nx.*wfq.*sJ.*(ffq));
+% Mf = Vfq'*diag(wfq)*Vfq;
+% LHSnodalIBP = Vfq'*diag(wfq.*nx)*Vfq*f - (diag(J.*rx)*Dr' + diag(J.*sx)*Ds' + diag(J.*tx)*Dt')*Mref*f;
 
 LHS = Vq'*(Jq.*wq.*(rxq.*(Vrq*f) + sxq.*(Vsq*f) + txq.*(Vtq*f)));
-LHSIBP = -(Vrq'*(rxq.*Jq.*wq.*(Vq*f)) + Vsq'*(sxq.*Jq.*wq.*(Vq*f)) + Vtq'*(txq.*Jq.*wq.*(Vq*f)));
-LHSIBP = LHSIBP + Vfq'*(nx.*wfq.*sJ.*(Vfq*f));
+
+LHSIBP = (Vrq'*(rxq.*Jq.*wq.*(Vq*f)) + Vsq'*(sxq.*Jq.*wq.*(Vq*f)) + Vtq'*(txq.*Jq.*wq.*(Vq*f)));
+LHSIBP = Vfq'*(wfq.*nx.*sJ.*(Vfq*f)) - LHSIBP;
 
 disp(sprintf('diff b/w nodal collocation and quadrature RHS: %g\n',norm(LHSnodal-LHS)))
 disp(sprintf('diff b/w quadrature and IBP RHS: %g\n',norm(LHS-LHSIBP)))
- % check IBP - why doesn't it work for mapped domain?
-% sum(wfq.*sJ)
 
 rxJ = reshape(rx.*J,(N+1)*(N+2)/2,N+1);
 sxJ = reshape(sx.*J,(N+1)*(N+2)/2,N+1);
@@ -113,6 +120,8 @@ rzJ = reshape(rz.*J,(N+1)*(N+2)/2,N+1);
 szJ = reshape(sz.*J,(N+1)*(N+2)/2,N+1);
 tzJ = reshape(tz.*J,(N+1)*(N+2)/2,N+1);
 
+J = reshape(J,(N+1)*(N+2)/2,N+1);
+
 r = reshape(r,(N+1)*(N+2)/2,N+1);
 s = reshape(s,(N+1)*(N+2)/2,N+1);
 t = reshape(t,(N+1)*(N+2)/2,N+1);
@@ -120,27 +129,33 @@ t = reshape(t,(N+1)*(N+2)/2,N+1);
 % plot3(rfq(fids{2}),sfq(fids{2}),nx(fids{2}).*sJ(fids{2}),'s')
 % plot3(rfq(fids{3}),sfq(fids{3}),nx(fids{3}).*sJ(fids{3}),'s')
 % plot3(sfq(fids{4}),tfq(fids{4}),nx(fids{4}).*sJ(fids{4}),'s')
-return
 
-% Vq = wedge_basis(N,rq,sq,tq);
+Vq = wedge_basis(N,rq,sq,tq)/V;
+M = Vq'*diag(wq)*Vq;
 
-fids = find(abs(s+1)<1e-8);
-Vft = wedge_basis(N,r(fids),s(fids),t(fids))/V;
-Vtri = Vandermonde2D(N,r(fids),t(fids));
+% triangular face s = -1
+rf = rtri; sf = -ones((N+1)*(N+2)/2,1); tf = stri;
+Vft = wedge_basis(N,rf,sf,tf)/V;
+Vtri = Vandermonde2D(N,rtri,stri);
 Mt = inv(Vtri*Vtri');
-
 Ltri = M\(Vft'*Mt);
 Ltri(abs(Ltri)<1e-8) = 0;
 
-fids = find(abs(t+1)<1e-8);
-Vfq = wedge_basis(N,r(fids),s(fids),t(fids))/V;
-V1D = Vandermonde1D(N,JacobiGL(0,0,N));
-V1Dq = Vandermonde1D(N,r1Dq)/V1D;
-M1D = V1Dq'*diag(w1Dq)*V1Dq;
-Mq = kron(M1D,M1D);
+% quadrilateral face t = -1
+[rquad squad] = meshgrid(r1Dq,r1D); rquad = rquad(:); squad = squad(:);
+plot(rquad,squad,'o');text(rquad(:),squad(:),num2str((1:length(rquad(:)))'));return
+rf = rquad; sf = squad; tf = -ones(size(rquad));
+Vfq = wedge_basis(N,rf,sf,tf)/V;
+Vquad = Vandermonde2DQuad(N,rquad,squad); 
+Mq = inv(Vquad*Vquad'); % exact mass matrices
 
 Lquad = M\(Vfq'*Mq);
 Lquad(abs(Lquad)<1e-8) = 0;
+
+% check metric identities
+norm(Dr*rxJ(:) + Ds*sxJ(:) + Dt*txJ(:))
+norm(Dr*ryJ(:) + Ds*syJ(:) + Dt*tyJ(:))
+norm(Dr*rzJ(:) + Ds*szJ(:) + Dt*tzJ(:))
 
 return
 %% pyramids
