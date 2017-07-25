eb895a08556ff14049df2e6cb6ee87918775a637
Jesse Chan <jchan985@gmail.com>
2017-07-25 17:29:46 -0500

    adding logmean

---
+++ matlab/cfd/logmean.m
@@ -0,0 +1,13 @@
+% computes (aL-aR) / (log(aL)-log(aR))
+
+function val = logmean(aL,aR)
+
+xi = aL./aR;
+f = (xi-1)./(xi+1);
+u = f.^2;
+
+F = log(xi)./2./f;
+ids = abs(u) < 1e-3; % arbitrary
+F(ids) = 1 + u(ids)/3 + u(ids).*u(ids)/5 + u(ids).*u(ids).*u(ids)/7;
+
+val = (aL+aR)./(2*F);
