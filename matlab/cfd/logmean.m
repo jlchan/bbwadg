43d58e774f0f8c781d8e7c240b6f82a17be351a9
Jesse Chan <jchan985@gmail.com>
2017-07-29 17:12:13 -0500

    updating cfd matlab files

--- matlab/cfd/logmean.m
+++ matlab/cfd/logmean.m
@@ -7,7 +7,7 @@ f = (xi-1)./(xi+1);
 u = f.^2;
 
 F = log(xi)./2./f;
-ids = abs(u) < 1e-3; % arbitrary
+ids = abs(u) < 1e-4; % arbitrary
 F(ids) = 1 + u(ids)/3 + u(ids).*u(ids)/5 + u(ids).*u(ids).*u(ids)/7;
 
 val = (aL+aR)./(2*F);
