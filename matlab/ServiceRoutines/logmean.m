dace709d6c70957b3541842d8248224158b272be
Jesse Chan <jchan985@gmail.com>
2019-09-25 22:33:32 -0500

    updating lgomean

--- matlab/ServiceRoutines/logmean.m
+++ matlab/ServiceRoutines/logmean.m
@@ -1,13 +1,42 @@
 % computes (aL-aR) / (log(aL)-log(aR))
 
-function val = logmean(aL,aR)
+function val = logmean(aL,aR,logL,logR)
 
-xi = aL./aR;
-f = (xi-1)./(xi+1);
-u = f.^2;
+if nargin == 2   
+    
+    xi = aL./aR;
+    f = (xi-1)./(xi+1);
+    u = f.^2;
+    ids = abs(u) < 1e-4; % arbitrary
+    ui = u(ids);
+    uu = ui.*ui;
+    uuu = ui.*uu;
+    uuuu = ui.*uuu;
+    F = zeros(size(aL));
+    F(~ids) = log(xi(~ids))./2./f(~ids);
+    F(ids) = 1 + ui*.333333333333 + uu*.2 + uuu*0.142857142857143 + uuuu*.11111111111111;
+    val = (aL+aR)./(2*F);
+    
+else    
+    % andrew winters approach
+    da = aR-aL;
+    aavg = .5*(aR+aL);
+    f = da./aavg;
+    v = f.^2;
+    ids = v < 1e-4;
+    vv = v(ids);
+    val = zeros(size(aL));
+    val(~ids) = (aL(~ids)-aR(~ids))./(logL(~ids)-logR(~ids));
+    val(ids) = aavg(ids).*(1 + vv.*(-.2-vv.*(.0512 - vv*0.026038857142857)));
+%     v2 = v(ids).*v(ids);
+%     val(ids) = aavg(ids).*(1 - v(ids).*.2-v2.*.0512 + v2.*v(ids)*0.026038857142857);
+end
+% gamma = 1.4;
+    % c1 = (gamma-2)/3;
+    % c2 = (gamma+1)*(gamma-2)*(gamma-3)/45;
+    % c3 = (gamma+1)*(gamma-2)*(gamma-3)*(2*gamma*(gamma-2)-9)/945;
+    
 
-F = log(xi)./2./f;
-ids = abs(u) < 1e-6; % arbitrary
-F(ids) = 1 + u(ids)/3 + u(ids).^2/5 + u(ids).^3/7 + u(ids).^4/9;
 
-val = (aL+aR)./(2*F);
+
+
