bb67e092a858b33a671481a57ec8845c3d929041
Jesse Chan <jchan985@gmail.com>
2020-01-04 09:53:38 -0600

    updates to paper

--- docs/ESDG_mortar/figs/plot_mortar.m
+++ docs/ESDG_mortar/figs/plot_mortar.m
@@ -18,45 +18,46 @@ sf2 = r1D2;
 % rf2 = [r1D2; e; r1D; -e];
 % sf2 = [e2; r1D; -e; r1D];
 
-% mortar
-a = .33;
-b = .75;
 
-% nonmortar values
-b = .33;
-
+hold on    
 plot([-1 1 1 -1 -1],[-1 -1 1 1 -1],'k-','linewidth',2)
-hold on
 plot(rq,sq,'ko','linewidth',2,'markersize',12,'MarkerFaceColor',[.49 1 .63])
 plot(rq,sq,'ko','linewidth',2,'markersize',16,'MarkerFaceColor',[.49 1 .63])
-% plot(a+rf,sf,'k-','linewidth',2)
-% plot(a+rf,sf,'rs','linewidth',2,'markersize',12,'MarkerFaceColor',[.49 1 .63])
 
-
-plot(b+rf2(1:N+1),sf2(1:N+1),'k-','linewidth',2)
-plot(b+rf2(N+2:end),sf2(N+2:end),'k-','linewidth',2)
-plot(b+rf2,sf2,'bs','linewidth',2,'markersize',12,'MarkerFaceColor',[.49 1 .63])
-
-% non-mortar coupling
-rfi = rf2(2)+b;
-sfi = sf2(N+1+3);
-for i = 1:length(rq(:))
-    plot([rq(i) .97*rfi],[sq(i) sfi],'r--','linewidth',2)    
+if 0
+    % % nonmortar values
+    b = .33;
+    % non-mortar coupling
+    rfi = rf2(2)+b;
+    sfi = sf2(N+1+3);
+    for i = 1:length(rq(:))
+        plot([rq(i) .97*rfi],[sq(i) sfi],'r--','linewidth',2)
+    end
+    plot(rfi,sfi,'ro','linewidth',2,'markersize',20)
+    
+else
+    % mortar
+    a = .7;
+    b = 1.4;
+    plot(a+rf,sf,'k-','linewidth',2)
+    plot(a+rf,sf,'rs','linewidth',2,'markersize',12,'MarkerFaceColor',[.49 1 .63])
+    plot(b+rf2(1:N+1),sf2(1:N+1),'k-','linewidth',2)
+    plot(b+rf2(N+2:end),sf2(N+2:end),'k-','linewidth',2)
+    plot(b+rf2,sf2,'bs','linewidth',2,'markersize',12,'MarkerFaceColor',[.49 1 .63])
+    
+%     % draw mortar coupling
+%     rfi = rf2(2)+b;
+%     sfi = sf2(N+1+3);
+%     for i = 1:N+1
+%         plot([a+rf(i) .97*rfi],[sf(i) sfi],'r--','linewidth',2)
+%     end
+%     plot(rfi,sfi,'ro','linewidth',2,'markersize',20)
 end
-plot(rfi,sfi,'ro','linewidth',2,'markersize',20)
 
-% % mortar coupling
-% rfi = rf2(2)+b;
-% sfi = sf2(N+1+3);
-% for i = 1:N+1
-%     plot([a+rf(i) .97*rfi],[sf(i) sfi],'r--','linewidth',2)    
-% end
-% plot(rfi,sfi,'ro','linewidth',2,'markersize',20)
 
-% 
 % plot(.5*([-1 1 1 -1 -1] + 1) + 1 + 2*b,.5*[-1 -1 1 1 -1]-.5,'k-','linewidth',2)
 % plot(.5*([-1 1 1 -1 -1] + 1) + 1 + 2*b,.5*[-1 -1 1 1 -1]+.5,'k-','linewidth',2)
 axis equal
 axis off
-print(gcf,'-dpng','~/Desktop/nonmortar_coupling.png')
+% print(gcf,'-dpng','~/Desktop/nonmortar_coupling.png')
 % print(gcf,'-dpng','~/Desktop/bbwadg/talks/RPI_2018/figs/mortar1.png')
